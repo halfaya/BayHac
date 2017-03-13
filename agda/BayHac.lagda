f89d7ba9f12788ba6e672b7ddbcf2768d3fc3a50
John Leo <leo@halfaya.org>
2017-03-13 12:56:33 -0700

    fix unicode

--- agda/BayHac.lagda
+++ agda/BayHac.lagda
@@ -1,24 +1,24 @@
 %<*nat>
 \begin{code}
 data ℕ : Set where
-  zero : ℕ
-  suc  : ℕ → ℕ
+  zero  :  ℕ
+  suc   :  ℕ → ℕ
 \end{code}
 %</nat>
 
 %<*vec>
 \begin{code}
 data Vec (A : Set) : ℕ → Set where
-  []  : Vec A zero
-  _::_ : {n : ℕ} → A → Vec A n → Vec A (suc n)
+  []   :  Vec A zero
+  _∷_  :  {n : ℕ} → A → Vec A n → Vec A (suc n)
 \end{code}
 %</vec>
 
 %<*plus>
 \begin{code}
 _+_ : ℕ → ℕ → ℕ
-zero  + n = n
-suc m + n = suc (m + n)
+zero   +  n  =  n
+suc m  +  n  =  suc (m + n)
 \end{code}
 %</plus>
 
@@ -26,33 +26,33 @@ suc m + n = suc (m + n)
 \begin{code}
 _++_ : {A : Set} → {m n : ℕ} →
        Vec A m → Vec A n → Vec A (m + n)
-[]       ++ y = y
-(x :: xs) ++ y = x :: (xs ++ y)
+[]        ++  y  =  y
+(x ∷ xs)  ++  y  =  x ∷ (xs ++ y)
 \end{code}
 %</append>
 
 %<*fin>
 \begin{code}
 data Fin : ℕ → Set where
-  zero : {n : ℕ}         → Fin (suc n)
-  suc  : {n : ℕ} → Fin n → Fin (suc n)
+  zero  :  {n : ℕ}          → Fin (suc n)
+  suc   :  {n : ℕ} → Fin n  → Fin (suc n)
 \end{code}
 %</fin>
 
 %<*lookup>
 \begin{code}
 lookup : {A : Set} → {n : ℕ} → Fin n → Vec A n → A
-lookup ()      []  -- can be omitted
-lookup zero    (x :: _ ) = x
-lookup (suc n) (_ :: xs) = lookup n xs
+lookup  ()       []           -- can be omitted
+lookup  zero     (x ∷ _ )  =  x
+lookup  (suc n)  (_ ∷ xs)  =  lookup n xs
 \end{code}
 %</lookup>
 
 %<*bool>
 \begin{code}
 data Bool : Set where
-  true  : Bool
-  false : Bool
+  true   :  Bool
+  false  :  Bool
 \end{code}
 %</bool>
 
@@ -66,9 +66,9 @@ data _≡_ {A : Set} : A → A → Set where
 %<*lt>
 \begin{code}
 _<_ : ℕ → ℕ → Bool
-_     < zero  = false
-zero  < suc n = true
-suc m < suc n = m < n
+_      <  zero   =  false
+zero   <  suc n  =  true
+suc m  <  suc n  =  m < n
 \end{code}
 %</lt>
 
@@ -76,16 +76,16 @@ suc m < suc n = m < n
 \begin{code}
 lookup' : {A : Set} → {n : ℕ} →
           (m : ℕ) → m < n ≡ true → Vec A n → A
-lookup' _       ()   []  -- required
-lookup' zero    refl (x :: _ ) = x
-lookup' (suc m) p    (_ :: xs) = lookup' m p xs
+lookup' _        ()    []           -- required
+lookup' zero     refl  (x ∷ _ )  =  x
+lookup' (suc m)  p     (_ ∷ xs)  =  lookup' m p xs
 \end{code}
 %</lookup2>
 
 %<*infix>
 \begin{code}
 infixl 6 _+_
-infixr 5 _::_ _++_
+infixr 5 _∷_ _++_
 infix  4 _<_
 infix  2 _≡_
 \end{code}
--- src/Vector.hs
+++ src/Vector.hs
@@ -5,9 +5,9 @@ module Vector where
 import Prelude hiding ((++), lookup)
 import Data.Kind (Type)
 
-data Nat :: Type where
-  Zero :: Nat
-  Succ :: Nat -> Nat
+data Nat ∷ Type where
+  Zero ∷ Nat
+  Succ ∷ Nat → Nat
 
 instance Show Nat where
   show Zero     = "0"
@@ -15,63 +15,66 @@ instance Show Nat where
 
 n = Succ (Succ (Succ Zero))
 
-data Vec :: Type -> Nat -> Type where
-  VNil  :: ∀ (a :: Type).           Vec a 'Zero
-  VCons :: ∀ (a :: Type)(n :: Nat). a -> Vec a n -> Vec a ('Succ n)
+data Vec ∷ Type → Nat → Type where
+  Nil  ∷ ∀ (a ∷ Type).           Vec a 'Zero
+  (:>) ∷ ∀ (a ∷ Type)(n ∷ Nat). a → Vec a n → Vec a ('Succ n)
 
-instance ∀ (a :: Type)(n :: Nat). Show a => Show (Vec a n) where
-  show VNil         =  "[]"
-  show (VCons x xs) =  let end = case show xs of
-                             "[]"  ->  "]"
-                             ys    ->  concat [", ", tail ys]
+infixr 5 :>  
+infixr 4 ++  
+
+instance ∀ (a ∷ Type)(n ∷ Nat). Show a => Show (Vec a n) where
+  show Nil       =  "[]"
+  show (x :> xs) =  let end = case show xs of
+                             "[]"  →  "]"
+                             ys    →  concat [", ", tail ys]
                        in concat ["[", show x, end]
 
-x = VCons "a" ((VCons "b") VNil)
-y = VCons "c" ((VCons "d") ((VCons "e") VNil))
+x = "a" :> "b" :> Nil
+y = "c" :> "d" :> "e" :> Nil
 z = x ++ y
 
-type family (m :: Nat) + (n :: Nat) where
+type family (m ∷ Nat) + (n ∷ Nat) where
   Zero    + n = n
   'Succ m + n = 'Succ (m + n)
 
-(++) :: ∀ (a :: Type)(m :: Nat)(n :: Nat). Vec a m -> Vec a n -> Vec a (m + n)
-VNil      ++ v = v
-VCons x u ++ v = VCons x (u ++ v)
+(++) ∷ ∀ (a ∷ Type)(m ∷ Nat)(n ∷ Nat). Vec a m → Vec a n → Vec a (m + n)
+Nil    ++ v = v
+x :> u ++ v = x :> (u ++ v)
 
-data Fin :: Nat -> Type where
-  FZero :: ∀ (n :: Nat).          Fin (Succ n)
-  FSucc :: ∀ (n :: Nat). Fin n -> Fin (Succ n)
+data Fin ∷ Nat → Type where
+  FZero ∷ ∀ (n ∷ Nat).          Fin (Succ n)
+  FSucc ∷ ∀ (n ∷ Nat). Fin n → Fin (Succ n)
 
-lookup :: ∀ (a :: Type)(n :: Nat). Fin n -> Vec a n -> a
+lookup ∷ ∀ (a ∷ Type)(n ∷ Nat). Fin n → Vec a n → a
 --lookup  n          VNil          = undefined
-lookup FZero     (VCons x _)  = x
-lookup (FSucc n) (VCons _ xs) = lookup n xs
+lookup FZero     (x :> _)  = x
+lookup (FSucc n) (_ :> xs) = lookup n xs
 
 a = lookup (FSucc (FZero)) x
 
-data (a :: k) :~: (b :: k) where
-  Refl :: ∀ (k :: Type)(a :: k). a :~: a
+data (a ∷ k) :~: (b ∷ k) where
+  Refl ∷ ∀ (k ∷ Type)(a ∷ k). a :~: a
 
 infix 4 :~:
 infix 5 <
 
-type family (m :: Nat) < (n :: Nat) where
+type family (m ∷ Nat) < (n ∷ Nat) where
   _         < Zero      = False
   'Zero     < ('Succ _) = True
   ('Succ m) < ('Succ n) = m < n
 
-data SNat :: Nat -> Type where
-  SZero :: SNat 'Zero
-  SSucc :: ∀ (n :: Nat). SNat n -> SNat ('Succ n)
+data SNat ∷ Nat → Type where
+  SZero ∷ SNat 'Zero
+  SSucc ∷ ∀ (n ∷ Nat). SNat n → SNat ('Succ n)
 
---lookupBad :: ∀ (a :: Type)(m :: Nat)(n :: Nat). m -> m < n :~: True -> Vec a n -> a
+--lookupBad ∷ ∀ (a ∷ Type)(m ∷ Nat)(n ∷ Nat). m → m < n :~: True → Vec a n → a
 
-lookup' :: ∀ (a :: Type)(m :: Nat)(n :: Nat). SNat m -> m < n :~: True -> Vec a n -> a
-lookup' _         _     VNil        = undefined
-lookup' SZero     Refl (VCons x _)  = x
-lookup' (SSucc m) Refl (VCons _ xs) = lookup' m Refl xs
+lookup' ∷ ∀ (a ∷ Type)(m ∷ Nat)(n ∷ Nat). SNat m → m < n :~: True → Vec a n → a
+lookup' _         _     Nil      = undefined
+lookup' SZero     Refl (x :> _)  = x
+lookup' (SSucc m) Refl (_ :> xs) = lookup' m Refl xs
 
-nth :: ∀ (a :: Type)(m :: Nat)(n :: Nat). (m < n) ~ 'True => SNat m -> Vec a n -> a
+nth ∷ ∀ (a ∷ Type)(m ∷ Nat)(n ∷ Nat). (m < n) ~ 'True => SNat m → Vec a n → a
 --nth _         VNil         =  undefined
-nth SZero     (VCons a _)  = a
-nth (SSucc m) (VCons _ as) = nth m as
+nth SZero     (a :> _)  = a
+nth (SSucc m) (_ :> as) = nth m as
